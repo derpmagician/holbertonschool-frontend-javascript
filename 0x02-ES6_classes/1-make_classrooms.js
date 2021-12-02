62cf1529b37b8a6f22c228ef2cc170736236de90
derpmagician <renzin@gmail.com>
2021-12-02 17:13:28 +0000

    fix misstype

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
-import ClassRoom  from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const classRoomList = [];
-  const c1 = new ClassRoom(19);
-  const c2 = new ClassRoom(20);
-  const c3 = new ClassRoom(34);
-  classRoomList.push(c1, c2, c3);
-  return classRoomList;
+  const classroomList = [];
+  const cl1 = new ClassRoom(19);
+  const cl2 = new ClassRoom(20);
+  const cl3 = new ClassRoom(34);
+  classroomList.push(cl1, cl2, cl3);
+  return classroomList;
 }
