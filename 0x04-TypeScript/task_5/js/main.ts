d1b1fddc381838d690115e0997d33bf946db01c7
derpmagician <renzin@gmail.com>
2021-12-09 00:07:43 +0000

    fixing variables

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,34 +1,28 @@
 interface MajorCredits {
-  credit: number;
-  _brand: "Major";
+  credits: number;
+  brand: "Major";
 }
 
 interface MinorCredits {
-  credit: number;
-  _brand: "Minor";
+  credits: number;
+  brand: "Minor";
 }
 
-function sumMajorCredits(
-  subject1: MajorCredits,
-  subject2: MajorCredits
-): MajorCredits {
-  const sum = subject1.credit + subject2.credit;
+function sumMajorCredits( subject1: MajorCredits, subject2: MajorCredits ): MajorCredits {
+  const sum = subject1.credits + subject2.credits;
   const obj: MajorCredits = {
-    credit: sum,
-    _brand: "Major",
+    credits: sum,
+    brand: "Major",
   };
 
   return obj;
 }
 
-function sumMinorCredits(
-  subject1: MinorCredits,
-  subject2: MinorCredits
-): MinorCredits {
-  const sum = subject1.credit + subject2.credit;
+function sumMinorCredits( subject1: MinorCredits, subject2: MinorCredits ): MinorCredits {
+  const sum = subject1.credits + subject2.credits;
   const obj: MinorCredits = {
-    credit: sum,
-    _brand: "Minor",
+    credits: sum,
+    brand: "Minor",
   };
 
   return obj;
